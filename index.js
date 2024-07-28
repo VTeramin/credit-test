149e5c7aca84b1229eda22572e5cb0f0b443dba0
Valery Voinilko <vteramin@gmail.com>
2024-07-28 23:47:37 +0300

    feat: add button styles

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import creditsData from './credits-data.js';
 const creditsInfo = document.getElementById('credits-info');
 creditsData.forEach(credData => {
     const row = document.createElement('tr');
-    row.innerHTML = `<td>${credData.name}</td><td>${credData.rate}</td><td>${credData.term}</td><button></button>`;
+    row.innerHTML = `<td>Кредит "${credData.name}"</td><td>${credData.rate}</td><td>${credData.term}</td><td><button>Оформить заявку</button></td>`;
     creditsInfo.append(row);
 });
